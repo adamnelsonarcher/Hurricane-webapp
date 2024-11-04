e7a9de62514b90948dbc99a2a7a166d7a0a765cf
Adam Nelson-Archer <47440321+adamnelsonarcher@users.noreply.github.com>
2024-11-03 22:49:33 -0600

    added support for embed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,29 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Allow requests from your main domain
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://nelsonarcher.com',  // Your main domain
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
